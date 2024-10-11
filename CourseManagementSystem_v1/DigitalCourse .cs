cd8e649dba9165353bb0b76c38d44a1670b5e23f
UT01314 <UT01314@UNICOMTIC60>
2024-10-11 15:32:06 +0530

    quesin 6 a,b done

---
+++ CourseManagementSystem_v1/DigitalCourse .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class DigitalCourse:Course
+    {
+        public string CourseLink;
+        public int FileSize;
+
+        public DigitalCourse(string courseId, string title, string Duration,decimal Price, string courseLink, int fileSize):base(courseId, title, Duration, Price)
+        {
+            CourseLink = courseLink;
+            FileSize = fileSize;
+        }
+    }
+}
---
+++ CourseManagementSystem_v1/OnPremisCourse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CourseManagementSystem_v1
+{
+    internal class OnPremisCourse:Course
+
+    {
+
+        public string Schedule;
+        public int ClassroomCapacity;
+
+        public OnPremisCourse(string courseId, string title, string Duration, decimal Price, string schedule, int classroomCapacity):base(courseId, title, Duration, Price)
+        {
+            Schedule = schedule;
+            ClassroomCapacity = classroomCapacity;
+        }
+    }
+}
