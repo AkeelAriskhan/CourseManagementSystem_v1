6467c01b8ecabd5becb6e46505c4ec872bc95645
UT01314 <UT01314@UNICOMTIC60>
2024-10-11 14:12:25 +0530

    Add project files.

---
+++ CourseManagementSystem_v1/Course .cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+    {
+
+          private string courseId {  get; set; }
+           private string title { get; set; }
+           private string Duration { get; set; }
+            private decimal Price { get; set; }
+
+        public Course(string courseId, string title, string duration, decimal price)
+        {
+            this.courseId = courseId;
+            this.title = title;
+            Duration = duration;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"courseId:{courseId}  title :{title} Duration:{Duration} Price: {Price}";
+        }
+    }
+}
---
+++ CourseManagementSystem_v1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course1 = new Course("C_001", "phython", "3months", 10);
+            Console.WriteLine(course1.ToString());
+        }
+    }
+}
